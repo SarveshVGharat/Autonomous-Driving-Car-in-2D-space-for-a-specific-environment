652b20722eb923cd1eb8d501cf34ce4177491f4d
Sarvesh Gharat <89913681+SarveshVGharat@users.noreply.github.com>
2022-12-09 00:20:04 +0530

    Add files via upload

---
+++ gym_driving/simulator/run_simulator.py
@@ -0,0 +1,337 @@
+from importlib.resources import path
+from gym_driving.assets.car import *
+from gym_driving.envs.environment import *
+from gym_driving.envs.driving_env import *
+from gym_driving.assets.terrain import *
+
+import time
+import pygame, sys
+from pygame.locals import *
+import random
+import math
+import argparse
+
+# Do NOT change these values
+TIMESTEPS = 1000
+FPS = 30
+NUM_EPISODES = 10
+
+class Task1():
+
+    def __init__(self):
+        """
+        Can modify to include variables as required
+        """
+
+        super().__init__()
+
+    def next_action(self, state, theta, theta_prime):
+        """
+        Input: The current state
+        Output: Action to be taken
+        TO BE FILLED
+        """
+
+        # Replace with your implementation to determine actions to be taken
+        if abs(theta_prime  - theta) < 3.2:
+            action_steer = 1
+            action_acc = 4
+            action = np.array([action_steer, action_acc])  
+
+        else:
+            action_steer = 2
+            action_acc = 2
+            action = np.array([action_steer, action_acc]) 
+        return action
+
+    def controller_task1(self, config_filepath=None, render_mode=False):
+        """
+        This is the main controller function. You can modify it as required except for the parts specifically not to be modified.
+        Additionally, you can define helper functions within the class if needed for your logic.
+        """
+    
+        ######### Do NOT modify these lines ##########
+        pygame.init()
+        fpsClock = pygame.time.Clock()
+
+        if config_filepath is None:
+            config_filepath = '../configs/config.json'
+
+        simulator = DrivingEnv('T1', render_mode=render_mode, config_filepath=config_filepath)
+
+        time.sleep(3)
+        ##############################################
+
+        # e is the number of the current episode, running it for 10 episodes
+        for e in range(NUM_EPISODES):
+        
+            ######### Do NOT modify these lines ##########
+            
+            # To keep track of the number of timesteps per epoch
+            cur_time = 0
+
+            # To reset the simulator at the beginning of each episode
+            state = simulator._reset()
+
+            x, y = state[0], -state[1]
+            theta_prime = math.atan((-30-y)/(350-x))
+            theta_prime = np.rad2deg(theta_prime)
+            if theta_prime < 0:
+                theta_prime = abs(theta_prime)
+            else:
+                theta_prime = 360 - theta_prime 
+            
+            # Variable representing if you have reached the road
+            road_status = False
+            ##############################################
+
+            # The following code is a basic example of the usage of the simulator
+            for t in range(TIMESTEPS):
+        
+                # Checks for quit
+                if render_mode:
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            pygame.quit()
+                            sys.exit()
+
+                theta = state[3]
+                action = self.next_action(state, theta, theta_prime)
+                state, reward, terminate, reached_road, info_dict = simulator._step(action)
+                fpsClock.tick(FPS)
+
+                cur_time += 1
+
+                if terminate:
+                    road_status = reached_road
+                    break
+
+            # Writing the output at each episode to STDOUT
+            print(str(road_status) + ' ' + str(cur_time))
+
+class Task2():
+
+    def __init__(self):
+        """
+        Can modify to include variables as required
+        """
+
+        super().__init__()
+
+    def controller_task2(self, config_filepath=None, render_mode=False):
+        """
+        This is the main controller function. You can modify it as required except for the parts specifically not to be modified.
+        Additionally, you can define helper functions within the class if needed for your logic.
+        """
+        
+        ################ Do NOT modify these lines ################
+        pygame.init()
+        fpsClock = pygame.time.Clock()
+
+        if config_filepath is None:
+            config_filepath = '../configs/config.json'
+
+        time.sleep(3)
+        ###########################################################
+
+        # e is the number of the current episode, running it for 10 episodes
+        for e in range(NUM_EPISODES):
+
+            ################ Setting up the environment, do NOT modify these lines ################
+            # To randomly initialize centers of the traps within a determined range
+            ran_cen_1x = random.randint(120, 230)
+            ran_cen_1y = random.randint(120, 230)
+            ran_cen_1 = [ran_cen_1x, ran_cen_1y]
+
+            ran_cen_2x = random.randint(120, 230)
+            ran_cen_2y = random.randint(-230, -120)
+            ran_cen_2 = [ran_cen_2x, ran_cen_2y]
+
+            ran_cen_3x = random.randint(-230, -120)
+            ran_cen_3y = random.randint(120, 230)
+            ran_cen_3 = [ran_cen_3x, ran_cen_3y]
+
+            ran_cen_4x = random.randint(-230, -120)
+            ran_cen_4y = random.randint(-230, -120)
+            ran_cen_4 = [ran_cen_4x, ran_cen_4y]
+
+            ran_cen_list = [ran_cen_1, ran_cen_2, ran_cen_3, ran_cen_4]            
+            eligible_list = []
+
+            # To randomly initialize the car within a determined range
+            for x in range(-300, 300):
+                for y in range(-300, 300):
+
+                    if x >= (ran_cen_1x - 110) and x <= (ran_cen_1x + 110) and y >= (ran_cen_1y - 110) and y <= (ran_cen_1y + 110):
+                        continue
+
+                    if x >= (ran_cen_2x - 110) and x <= (ran_cen_2x + 110) and y >= (ran_cen_2y - 110) and y <= (ran_cen_2y + 110):
+                        continue
+
+                    if x >= (ran_cen_3x - 110) and x <= (ran_cen_3x + 110) and y >= (ran_cen_3y - 110) and y <= (ran_cen_3y + 110):
+                        continue
+
+                    if x >= (ran_cen_4x - 110) and x <= (ran_cen_4x + 110) and y >= (ran_cen_4y - 110) and y <= (ran_cen_4y + 110):
+                        continue
+
+                    eligible_list.append((x,y))
+
+            simulator = DrivingEnv('T2', eligible_list, render_mode=render_mode, config_filepath=config_filepath, ran_cen_list=ran_cen_list)
+        
+            # To keep track of the number of timesteps per episode
+            cur_time = 0
+
+            # To reset the simulator at the beginning of each episode
+            state = simulator._reset(eligible_list=eligible_list)
+            ###########################################################
+
+            # The following code is a basic example of the usage of the simulator
+            road_status = False
+            Flag = True
+
+            for t in range(TIMESTEPS):
+        
+                # Checks for quit
+                if render_mode:
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            pygame.quit()
+                            sys.exit()
+
+                x, y = state[0], -state[1]
+                            
+                if y > -60 and y < 60:
+                    if Flag:
+                        theta = state[3]
+                        theta_prime = math.atan((-30-y)/(350-x))
+                        theta_prime = np.rad2deg(theta_prime)
+                        if theta_prime < 0:
+                            theta_prime = abs(theta_prime)
+                        else:
+                            theta_prime = 360 - theta_prime 
+
+                    if abs(theta_prime - theta) < 3.2:
+                        action_steer = 1
+                        action_acc = 4
+                        Flag = False
+                        action = np.array([action_steer, action_acc])  
+                    else:
+                        action_steer = 2
+                        action_acc = 2
+                        Flag = True
+                        action = np.array([action_steer, action_acc]) 
+
+                else:
+                    Flag = True
+                    if y < 0:
+                        if (x > ran_cen_1x - 80 and x < ran_cen_1x + 80) or (x > ran_cen_3x - 80 and x < ran_cen_3x + 80):
+                            theta = state[3]
+                            theta_prime = math.atan((y-y)/(350-x))
+                            theta_prime = np.rad2deg(theta_prime)
+                            if theta_prime < 0:
+                                theta_prime = abs(theta_prime)
+                            else:
+                                theta_prime = 360 - theta_prime 
+
+                            if abs(theta_prime  - theta) < 5.2:
+                                action_steer = 1
+                                action_acc = 4
+                                action = np.array([action_steer, action_acc])  
+                            else:
+                                action_steer = 2
+                                action_acc = 2
+                                action = np.array([action_steer, action_acc]) 
+                        else:
+                            theta = state[3]
+                            theta_prime = np.pi/2 
+                            theta_prime = np.rad2deg(theta_prime)
+                            if theta_prime < 0:
+                                theta_prime = abs(theta_prime)
+                            else:
+                                theta_prime = 360 - theta_prime 
+
+                            if abs(theta_prime  - theta) < 5.2:
+                                action_steer = 1
+                                action_acc = 4
+                                action = np.array([action_steer, action_acc])  
+                            else:
+                                action_steer = 2
+                                action_acc = 2
+                                action = np.array([action_steer, action_acc]) 
+                    else:
+                        if (x > ran_cen_2x - 80 and x < ran_cen_2x + 80) or (x > ran_cen_4x - 80 and x < ran_cen_4x + 80):
+                            theta = state[3]
+                            theta_prime = math.atan((y-y)/(350-x))
+                            theta_prime = np.rad2deg(theta_prime)
+                            if theta_prime < 0:
+                                theta_prime = abs(theta_prime)
+                            else:
+                                theta_prime = 360 - theta_prime 
+
+                            if abs(theta_prime  - theta) < 5.2:
+                                action_steer = 1
+                                action_acc = 4
+                                action = np.array([action_steer, action_acc])  
+                            else:
+                                action_steer = 2
+                                action_acc = 2
+                                action = np.array([action_steer, action_acc]) 
+                        else:
+                            theta = state[3]
+                            theta_prime = 3*np.pi/2 
+                            theta_prime = np.rad2deg(theta_prime)
+                            if theta_prime < 0:
+                                theta_prime = abs(theta_prime)
+                            else:
+                                theta_prime = 360 - theta_prime 
+
+                            if abs(theta_prime  - theta) < 5.2:
+                                action_steer = 1
+                                action_acc = 4
+                                action = np.array([action_steer, action_acc])  
+                            else:
+                                action_steer = 2
+                                action_acc = 2
+                                action = np.array([action_steer, action_acc]) 
+
+                state, reward, terminate, reached_road, info_dict = simulator._step(action)
+                fpsClock.tick(FPS)
+
+                cur_time += 1
+
+                if terminate:
+                    road_status = reached_road
+                    break
+
+            print(str(road_status) + ' ' + str(cur_time))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="config filepath", default=None)
+    parser.add_argument("-t", "--task", help="task number", choices=['T1', 'T2'])
+    parser.add_argument("-r", "--random_seed", help="random seed", type=int, default=0)
+    parser.add_argument("-m", "--render_mode", action='store_true')
+    parser.add_argument("-f", "--frames_per_sec", help="fps", type=int, default=30) # Keep this as the default while running your simulation to visualize results
+    args = parser.parse_args()
+
+    config_filepath = args.config
+    task = args.task
+    random_seed = args.random_seed
+    render_mode = args.render_mode
+    fps = args.frames_per_sec
+
+    FPS = fps
+
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+
+    if task == 'T1':
+        
+        agent = Task1()
+        agent.controller_task1(config_filepath=config_filepath, render_mode=render_mode)
+
+    else:
+
+        agent = Task2()
+        agent.controller_task2(config_filepath=config_filepath, render_mode=render_mode)
